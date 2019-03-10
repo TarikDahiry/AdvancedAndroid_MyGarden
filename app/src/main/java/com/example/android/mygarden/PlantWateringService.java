ea4a1cec131bcf81932810122609233bd15d840b
Tarik <topqal16@gmail.com>
2019-03-10 19:42:43 +0100

    Adding an IntentService

---
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,51 @@
+package com.example.android.mygarden;
+
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+import static com.example.android.mygarden.provider.PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME;
+import static com.example.android.mygarden.provider.PlantContract.PlantEntry.CONTENT_URI;
+
+public class PlantWateringService extends IntentService {
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
+
+    public PlantWateringService() {
+        super("PlantWateringService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) handleActionWaterPlants();
+        }
+    }
+
+    private void handleActionWaterPlants() {
+        //Uri PLANTS_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PLANTS);
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        ContentValues values = new ContentValues();
+        long currentime = System.currentTimeMillis();
+        values.put(COLUMN_LAST_WATERED_TIME, currentime);
+        // Update only plants that are still alive
+        getContentResolver().update(
+                PLANTS_URI,
+                values,
+                COLUMN_LAST_WATERED_TIME+">?",
+                new String[]{String.valueOf(currentime - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+}
