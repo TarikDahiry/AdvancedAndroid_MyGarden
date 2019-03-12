cc55a721bb2d362fe1314d00fdfec52d63ff38a6
Tarik <topqal16@gmail.com>
2019-03-12 23:35:49 +0100

    Replace loader deprecated method

--- app/src/main/java/com/example/android/mygarden/ui/MainActivity.java
+++ app/src/main/java/com/example/android/mygarden/ui/MainActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -42,24 +43,23 @@ public class MainActivity
     private static final int GARDEN_LOADER_ID = 100;
     private PlantListAdapter mAdapter;
 
-    private RecyclerView mGardenRecyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // The main activity displays the garden as a grid layout recycler view
-        mGardenRecyclerView = (RecyclerView) findViewById(R.id.plants_list_recycler_view);
+        RecyclerView mGardenRecyclerView = findViewById(R.id.plants_list_recycler_view);
         mGardenRecyclerView.setLayoutManager(
                 new GridLayoutManager(this, 4)
         );
         mAdapter = new PlantListAdapter(this, null);
         mGardenRecyclerView.setAdapter(mAdapter);
 
-        getSupportLoaderManager().initLoader(GARDEN_LOADER_ID, null, this);
+        LoaderManager.getInstance(this).initLoader(GARDEN_LOADER_ID, null, this);
     }
 
+    @NonNull
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         Uri PLANT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
@@ -68,18 +68,18 @@ public class MainActivity
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
         cursor.moveToFirst();
         mAdapter.swapCursor(cursor);
     }
 
     @Override
-    public void onLoaderReset(Loader loader) {
+    public void onLoaderReset(@NonNull Loader loader) {
 
     }
 
     public void onPlantClick(View view) {
-        ImageView imgView = (ImageView) view.findViewById(R.id.plant_list_item_image);
+        ImageView imgView = view.findViewById(R.id.plant_list_item_image);
         long plantId = (long) imgView.getTag();
         Intent intent = new Intent(getBaseContext(), PlantDetailActivity.class);
         intent.putExtra(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
